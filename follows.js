4cf765cb63355ad35090e86327e0590113f8cf71
Canyardo <Canyardo@users.noreply.github.com>
2014-02-16 13:06:40 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= '4tzMccEyqECx4LA5XAyfQ1euYeEyZz4p9pcp7UCsE5ywlq10', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [452301631!SensorLDR!AjusteLDR], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'La Web de Odoacro el Herulo', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
